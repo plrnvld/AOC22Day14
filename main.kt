88c18a10c68bd01f704ef615114661d6f8b97b0c
Paul Rijneveld <paulrijneveld@gmail.com>
2023-01-02 19:23:14 +0000

    Read file

--- main.kt
+++ main.kt
@@ -1,3 +1,16 @@
-fun main(args: Array<String>) {
-  println("Hello world!")
+import java.io.*
+
+class Calculator { 
+    fun readFileAsLines(fileName: String): List<String>
+       = File(fileName).bufferedReader().readLines()
+    /*...*/ 
 }
+
+fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
+  println("Hello world!")
+
+  val lines = Calculator().readFileAsLines("Example.txt")
+  lines.forEach{it -> println("$it")};      
+ 
+}
+
